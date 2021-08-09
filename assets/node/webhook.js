c211fb975d7f146087084d6ebb6475065e9316e5
sean brogan <seanmaxbrogan@gmail.com>
2021-08-09 08:27:14 -0700

    commit

--- assets/node/webhook.js
+++ assets/node/webhook.js
@@ -1,16 +1,16 @@
-const repo = '/var/www/bytfix'
-const secret = require("./.secret.json").secret;
-const http = require("http")
-const crypto = require("crypto")
-const { exec } = require("child_process");
-http.createServer(function (req, res) {
-    req.on('data', function(chunk) {
-        let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
-
-        if (req.headers['x-hub-signature'] == sig) {
-            exec('cd ' + repo + ' && sudo git pull --autostash');
-            console.log("pulling... ");
-        }
-    })
-    res.end()
-}).listen(8080);
+    const path = '/var/www/'
+    const secret = require("./.secret.json").secret;
+    const http = require("http")
+    const crypto = require("crypto")
+    const { exec } = require("child_process");
+    http.createServer(function (req, res) {
+        req.on('data', function(chunk) {
+            let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
+            let repo = chunk.repository.name;
+            if (req.headers['x-hub-signature'] == sig) {
+                exec('cd ' + path + repo + ' && sudo git pull --autostash');
+                console.log("pulling... ");
+            }
+        })
+        res.end()
+    }).listen(8080)
