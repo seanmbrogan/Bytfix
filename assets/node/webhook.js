7c44d00205590e169e8873955df50f3969ff5415
sean brogan <seanmaxbrogan@gmail.com>
2021-08-09 06:59:19 -0700

    commit

--- assets/node/webhook.js
+++ assets/node/webhook.js
@@ -6,11 +6,11 @@ http.createServer(function (req, res) {
     req.on('data', function(chunk) {
         
         let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
-
+        con
         if (req.headers['x-hub-signature'] == sig) {
             exec('cd ' + repo + ' && git pull');
             console.log("pulling... ");
         }
     });
     res.end()
-}).listen(8080);
+}).listen(8080);a
